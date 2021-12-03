4177744704e4095c5bfa36ae8d7bd17f96e49d3b
Saberrytwo <90337853+Saberrytwo@users.noreply.github.com>
2021-12-02 18:25:41 -0700

    pretty website

--- intexApp/admin.py
+++ intexApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import PdDrugs, PdPrescriber, PdPrescribersCredentials, PdStatedata, PdTriple
+
+admin.site.register(PdTriple)
+admin.site.register(PdDrugs)
+admin.site.register(PdPrescriber)
+admin.site.register(PdPrescribersCredentials)
+admin.site.register(PdStatedata)
